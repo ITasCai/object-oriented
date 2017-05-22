7024562330aa1e643ab89fc20e9cb42dc8a31de6
孤独的蔬菜 <shuai19970217@163.com>
2017-05-22 11:59:04 +0800

    5.22练习

---
+++ 5.19_lx/PhoneBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._19_lx
+{
+    class PhoneBook
+    {
+        private string name;
+        private int age;
+        
+    }
+}
